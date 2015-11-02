503bc79c561993ca24c2347b5f77a57755b2eb4c
Ramis Wachtler <rwachtler@mbp-von-ramis.fh-joanneum.at>
2015-11-02 14:35:59 +0100

    Implemented Toast notice when user selects a category instead of a location

--- MensaApp/app/src/main/java/itm/fhj/at/mensaapp/activity/LocationsList.java
+++ MensaApp/app/src/main/java/itm/fhj/at/mensaapp/activity/LocationsList.java
@@ -1,12 +1,14 @@
 package itm.fhj.at.mensaapp.activity;
 
 import android.app.ListActivity;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -23,16 +25,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class LocationsList extends ListActivity {
 
+    private ArrayList<String> retrievedLocations = new ArrayList<String>();
+
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        ArrayList<String> retrievedLocations = (ArrayList<String>)getIntent().getSerializableExtra("LOCATIONS");
+        this.retrievedLocations = (ArrayList<String>)getIntent().getSerializableExtra("LOCATIONS");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, retrievedLocations);
+                android.R.layout.simple_list_item_1, this.retrievedLocations);
 
         setListAdapter(adapter);
         getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
@@ -41,7 +46,17 @@ public class LocationsList extends ListActivity {
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        // Check if value not 0
-        // Store selected value
+
+        Location selectedLocation = (Location) getListAdapter().getItem(position);
+
+        if(selectedLocation.getId() > 0){
+            /*
+                - Store location here
+                - Dismiss this view or load new activity
+             */
+        } else{
+            Toast toast = Toast.makeText(this, "Sie können keine Kategorie auswählen!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
--- MensaApp/app/src/main/java/itm/fhj/at/mensaapp/model/Location.java
+++ MensaApp/app/src/main/java/itm/fhj/at/mensaapp/model/Location.java
@@ -38,6 +38,6 @@ public class Location implements Serializable{
 
     @Override
     public String toString() {
-        return this.name;
+        return this.name.replaceAll("&amp;", "&");
     }
 }

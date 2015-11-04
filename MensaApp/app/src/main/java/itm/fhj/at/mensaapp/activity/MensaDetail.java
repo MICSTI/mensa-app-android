cf955c870717a2ae1199115a61d8754dc2f0e83d
Michael Stifter <michael.stifter2@edu.fh-joanneum.at>
2015-11-04 12:51:23 +0100

    Changed demo JSON string to demo JSON object

--- MensaApp/app/src/main/java/itm/fhj/at/mensaapp/activity/MensaDetail.java
+++ MensaApp/app/src/main/java/itm/fhj/at/mensaapp/activity/MensaDetail.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -42,10 +43,59 @@ public class MensaDetail extends Activity {
         SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
 
         // TODO remove fake JSON and implement real parsing
-        String demoMealString = "{'id': 50, 'timestamp': 12345678, 'datetime': '26.10.2015T08:00:00', 'name': 'FH JOANNEUM Kapfenberg', 'mealSchedule': [{'date': '26.10.2015', 'info': 'Feiertag heute geschlossen'}, {'date': '27.10.2015', 'meals': [{'price': 4.30, 'description': 'Gemüsesupppe, Marillenknödel', 'type': 'Vegetarian'}, {'price': 4.90, 'description': 'Gemüsesuppe, Wiener Schnitzel', 'type': 'Classic'}, {'price': 4.90, 'description': 'Paprikaschnitzel mit Salzkartoffeln und Salat vom Buffet', 'type': 'Brainfood'}]}]}";
+
+        JSONObject demoMealSchedule = new JSONObject();
+
+        try {
+            demoMealSchedule.put("id", mensaId);
+            demoMealSchedule.put("timestamp", 12345678);
+            demoMealSchedule.put("datetime", "26.10.2015T08:00:00");
+            demoMealSchedule.put("name", "FH JOANNEUM Kapfenberg");
+
+            JSONArray mealSchedule = new JSONArray();
+
+            JSONObject day1 = new JSONObject();
+            day1.put("date", "26.10.2015");
+            day1.put("info", "Feiertag, heute geschlossen");
+
+            JSONObject day2 = new JSONObject();
+            day2.put("date", "27.10.2015");
+
+            JSONArray meals = new JSONArray();
+
+            JSONObject meal1 = new JSONObject();
+            meal1.put("price", 4.30);
+            meal1.put("description", "Gemüsesupppe, Marillenknödel");
+            meal1.put("type", "Vegetarian");
+
+            JSONObject meal2 = new JSONObject();
+            meal2.put("price", 4.90);
+            meal2.put("description", "Gemüsesuppe, Wiener Schnitzel");
+            meal2.put("type", "Classic");
+
+            JSONObject meal3 = new JSONObject();
+            meal3.put("price", 5.90);
+            meal3.put("description", "Paprikaschnitzel mit Salzkartoffeln und Salat vom Buffet");
+            meal3.put("type", "Brainfood");
+
+            meals.put(meal1);
+            meals.put(meal2);
+            meals.put(meal3);
+
+            day2.put("meals", meals);
+
+            mealSchedule.put(day1);
+            mealSchedule.put(day2);
+
+            demoMealSchedule.put("mealSchedule", mealSchedule);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(String.valueOf(mensaId), demoMealString.replace('\'', '"'));
+        editor.putString(String.valueOf(mensaId), demoMealSchedule.toString());
 
         String defaultString = "";
         String mealJson = preferences.getString(String.valueOf(mensaId), defaultString);

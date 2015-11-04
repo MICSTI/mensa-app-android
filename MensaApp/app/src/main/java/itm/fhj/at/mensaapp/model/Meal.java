fe14ca485d4c5e4114b4cf5e90db3028af7eba6c
Ramis Wachtler <rwachtler@mbp-von-ramis.fh-joanneum.at>
2015-11-04 11:41:33 +0100

    Implemented model class for Meal

---
+++ MensaApp/app/src/main/java/itm/fhj/at/mensaapp/model/Meal.java
@@ -0,0 +1,56 @@
+package itm.fhj.at.mensaapp.model;
+
+/**
+ * Created by rwachtler on 04.11.15.
+ */
+public class Meal {
+    String price = "";
+    String description = "";
+    String type = "";
+
+    /**
+     * Initializes a Meal object (case: all params are parsed from website)
+     * @param price - Price of the meal in EURO
+     * @param description - Textual description (e.g. Tomato soup, Schnitzel with rice)
+     * @param type - Vegetarian, Classic or Brainfood
+     */
+    public Meal(String price, String description, String type){
+        this.price = price;
+        this.description = description;
+        this.type = type;
+    }
+
+    /**
+     * Initializes a Meal object (case: only price and description are parsed from website)
+     * @param price - Price of the meal in EURO
+     * @param description - Textual description (e.g. Tomato soup, Schnitzel with rice)
+     */
+    public Meal(String price, String description){
+        this.price = price;
+        this.description = description;
+    }
+
+    /**
+     * Get the meal price
+     * @return price in EURO
+     */
+    public String getPrice() {
+        return price;
+    }
+
+    /**
+     * Get the meal description
+     * @return (e.g. Tomato soup, Schnitzel with rice)
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Get the meal type
+     * @return Vegetarian, Classic or Brainfood
+     */
+    public String getType() {
+        return type;
+    }
+}

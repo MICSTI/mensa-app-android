5bfcdeab9b39187b99d9b550f7314d8efeb72b7c
Michael Stifter <michael.stifter2@edu.fh-joanneum.at>
2015-11-06 16:26:25 +0100

    Started implementing favourite meals activity - added methods for getting and saving meals to SharedPreferences

--- MensaApp/app/src/main/java/itm/fhj/at/mensaapp/activity/FavouriteMealsList.java
+++ MensaApp/app/src/main/java/itm/fhj/at/mensaapp/activity/FavouriteMealsList.java
@@ -1,18 +1,46 @@
 package itm.fhj.at.mensaapp.activity;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import itm.fhj.at.mensaapp.R;
 
 public class FavouriteMealsList extends ListActivity {
 
+    // list containing the user's favourite meals
+    private ArrayList<String> favouriteMeals = new ArrayList<String>();
+
+    // reference to SharedPreferences
+    private SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+
+    // SharedPreferences key for favourite meals string
+    private static String KEY_FAVOURITE_MEALS = "favourite_meals";
+
+    // Delimiter character for saving the favourite meals string in SharedPreferences
+    private static String DELIMITER = ";";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite_meals_list);
+
+        // load favourite meals from SharedPreferences
+        getFavouriteMealsFromPreferences();
+
+        // populate list view
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, this.favouriteMeals);
+
+        setListAdapter(adapter);
+        getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
     }
 
 
@@ -36,5 +64,33 @@ public class FavouriteMealsList extends ListActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Loads the favourite meals from SharedPreferences.
+     */
+    private void getFavouriteMealsFromPreferences() {
+        String defaultValue = "";
+
+        String mealString = preferences.getString(KEY_FAVOURITE_MEALS, defaultValue);
+
+        String[] meals = mealString.split(DELIMITER);
+
+        for (String meal : meals)
+            this.favouriteMeals.add(meal);
+    }
+
+    /**
+     * Saves the favourite meals to SharedPreferences.
+     */
+    private boolean saveFavouriteMealsToPreferences() {
+        // build meal string from favourite meals list
+        String mealString = android.text.TextUtils.join(DELIMITER, this.favouriteMeals);
+
+        // save string to SharedPreferences
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(KEY_FAVOURITE_MEALS, mealString);
+
+        return editor.commit();
     }
 }
